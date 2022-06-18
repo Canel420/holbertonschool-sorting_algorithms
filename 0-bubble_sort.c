5057bde5b70434a9b1b419da7f04497f5bc049e8
Canel420 <4521@holbertonschool.com>
2022-06-18 13:00:12 -0500

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * swap - Swap to values 
+ * 
+ * @eft: value on the left.
+ * @right: value on the right.
+ * 
+ * Description: Takes to values and exchange
+ * its positions.
+ */
+void swap(int *left, int *right)
+{
+    int tmp = 0;
+
+    tmp = *left;
+    *left = *right;
+    *right = tmp;
+}
+
+/**
+ * bubble_sort - Function that sorts an array of integers. 
+ * 
+ * @array: array to sort.
+ * @size: size of the array.
+ * 
+ * Description:  sorts an array of integers in 
+ * ascending order using the Bubble sort algorithm. 
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i = 0, j = 0, swapped;
+
+    if (array == NULL || size < 2)
+        return;
+
+    for (; i < size-1; i++)
+    {
+        swapped = 0;
+        for (j = 0; j < size-i-1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap(&array[j], &array[j+1]);
+                swapped = 1;
+                print_array(array, size);
+            }
+        }
+        if (swapped == 0)
+            break;
+    }
+}
